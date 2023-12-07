26dacec4f1e47e1c7cca92761cc7ba41812a1c2e
writeb <r_amantaiuly@kbtu.kz>
2023-12-07 21:30:58 +0600

    Initial commit

---
+++ src/main/java/com/example/projectspring/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.projectspring.service;
+
+import com.example.projectspring.model.User;
+import com.example.projectspring.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> userDetail = repository.findByName(username);
+
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+
+    public String addUser(User userInfo) {
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+
+
+}
+
