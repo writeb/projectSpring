ff91382affaf96552f3957b006e605d7ad3ceca8
writeb <r_amantaiuly@kbtu.kz>
2023-12-07 21:36:45 +0600

    init

--- src/main/java/com/example/projectspring/model/User.java
+++ src/main/java/com/example/projectspring/model/User.java
@@ -1,14 +1,12 @@
 package com.example.projectspring.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "t_users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/example/projectspring/service/UserInfoDetails.java
+++ src/main/java/com/example/projectspring/service/UserInfoDetails.java
@@ -3,13 +3,14 @@ package com.example.projectspring.service;
 import com.example.projectspring.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UserInfoDetails implements UserInfoDetails {
+public class UserInfoDetails implements UserDetails {
 
     private String name;
     private String password;
